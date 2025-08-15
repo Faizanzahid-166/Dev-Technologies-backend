586f106fcf5d61cac60def6810957025bea8a2dd
Your Name <you@example.com>
2025-08-15 20:59:02 +0500

    fix

--- 01_ecommerce_backend/src/server.js
+++ 01_ecommerce_backend/src/server.js
@@ -11,7 +11,7 @@ import contactRoute from "./routes/contactRoute.js";
 
 const server = express();
 server.use(cors({
-    origin: process.env.CORS_ORIGIN ,
+    origin: "https://dev-technologies-frontend-omega.vercel.app" ,
     credentials: true,
      methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
