3db12f5fbab975137c02c8485f1a680b612caf5c
Your Name <you@example.com>
2025-08-16 11:07:20 +0500

    fix

--- 01_ecommerce_backend/src/server.js
+++ 01_ecommerce_backend/src/server.js
@@ -11,7 +11,11 @@ import contactRoute from "./routes/contactRoute.js";
 
 const server = express();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+  'http://localhost:5173', // local dev
+  'https://dev-technologies-frontend.vercel.app', // old frontend URL
+  'https://dev-technologies-frontend-vrnn.vercel.app' // new frontend URL
+];
 
 server.use(cors({
     origin: function (origin, callback) {
