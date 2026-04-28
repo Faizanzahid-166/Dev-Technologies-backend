a0f920b15339d92d7a42ed2bd6fff90781343469
Faizanzahid-166 <faizanzahid150@gmail.com>
2026-04-28 09:29:03 +0500

    fix

--- 02_Nextus/frontend/src/Routers.jsx
+++ 02_Nextus/frontend/src/Routers.jsx
@@ -5,16 +5,11 @@ import {Home,
          UserPannel, User,
          Admin,
          ChatPage,
-         Documents, //MyDocs,UploadDoc,Download,AddSignature,
-        // Profile,Listofusers,
-        // MeetingsPage, UserList,Messages,Chat,
-        // PaymentPage,PaymentForm,
+
         NotFound
        } from './pages/index.js'
 
 import {createBrowserRouter,} from 'react-router'
-// import ProtectedRoute from './components/ProtectedRoute.jsx';
-
 
 
 const routers = createBrowserRouter([
@@ -22,7 +17,6 @@ const routers = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      // { index: true, element: <ProtectedRoute><HomePage /></ProtectedRoute> },
     
        { path: "*", element: <NotFound /> },
 
@@ -46,36 +40,11 @@ const routers = createBrowserRouter([
            // 2-chat app
             { path: "/dashboard/user-pannel/chat/ChatPage", element: <ChatPage /> },
 
-           // 3- docomentation
-              { path: "/dashboard/user-pannel/documents", element: <Documents />,
-                children: [
-                  // { path: "/documents/upload", element: <UploadDoc /> },
-                  // { path: "/documents/list", element: <MyDocs /> },
-                  // { path: "/documents/signature", element: <AddSignature /> },
-                  // { path: "/documents/download", element: <Download /> },
-                  ] 
-              },
-
          ]
         },
     
 
       { path: "/dashboard/admin-pannel", element: <Admin /> },
-      //  { path: "/finduser", element: <Listofusers /> },
-
-
-      // //meeting
-      //  { path: "/messages", element: <ProtectedRoute><Messages /></ProtectedRoute>,
-      //   children:[
-      //   { path: "/messages/meetingArangment", element: <ProtectedRoute><MeetingsPage /></ProtectedRoute> },
-      //   { path: "/messages/videocall", element: <ProtectedRoute><UserList /></ProtectedRoute> },
-      //   { path: "/messages/chat", element: <ProtectedRoute><Chat /></ProtectedRoute> },
-      //   ]
-      //  },
-
-      //  { path: "/transactions", element: <PaymentPage /> },
-      //  { path: "/realtransactions", element: <PaymentForm /> },
-    
 
 
     ],
--- 02_Nextus/frontend/src/pages/index.js
+++ 02_Nextus/frontend/src/pages/index.js
@@ -13,25 +13,6 @@ import Home from './home/Home.jsx'
  
  import ChatPage from './chat/ChatPage.jsx'
 
-   import Documents from './documents/Documents.jsx'
- //  import MyDocs from './documents/DocLists.jsx'
-//  import UploadDoc from './documents/UploadDoc.jsx'
-//  import Download from './documents/Dowloaddoc.jsx'
-//  import AddSignature from './documents/Signature.jsx'
-//  import DocumentPreview from './documents/Pdfviewer.jsx'
-
- //  import Profile from './profile/Profile.jsx'
-//  import Listofusers from './profile/Listofusers.jsx'
-
-
-
-//  import MeetingsPage from './meetings/MeetingList.jsx'
-//  import UserList from './meetings/Videouserlist.jsx'
-//  import Chat from './meetings/Chat.jsx'
-
-//  import PaymentPage from './payments/PaymentForm.jsx'
-//  import PaymentForm from './payments/Realpayment.jsx'
-
  import NotFound from './Error.jsx'
 export {
     Home,
@@ -41,14 +22,6 @@ export {
      UserPannel,User,Admin,
 
      ChatPage,
-     
-     Documents,//MyDocs,UploadDoc,Download,AddSignature,DocumentPreview,
-    
-     // Profile,Listofusers,
-
-    // MeetingsPage,UserList,Messages,Chat, 
-    
-    // PaymentPage,PaymentForm,
 
     NotFound
 }
