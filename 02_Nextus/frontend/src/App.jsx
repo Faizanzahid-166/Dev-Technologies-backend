585ba888056980b0428b4ba273bfd8d12b8aaaba
Faizanzahid-166 <faizanzahid150@gmail.com>
2026-04-28 09:27:43 +0500

    delete extra files

--- 02_Nextus/frontend/src/App.jsx
+++ 02_Nextus/frontend/src/App.jsx
@@ -2,14 +2,13 @@ import React, { useState, useContext } from "react";
 import { Outlet } from "react-router"; // ✅ FIX
 import { Navbar, Footer } from "./layout/00_index.js";
 import ThemeContext from "./context/themeContext.js";
-import Health from './pages/Health.jsx'
 
 function App() {
   const { darkMode } = useContext(ThemeContext);
 
   return (
       <div className={darkMode ? "bg-gray-900 text-white" : "bg-white text-black"}>
-        <Health /> {/* Health check component to test backend connectivity */}  
+
       {/* Top Navbar */}
       <Navbar  />
       
