865afc118b89e546d7a4db2d7d7f1924c862906b
Faizanzahid-166 <faizanzahid150@gmail.com>
2026-05-10 02:23:18 +0500

    fiz

--- 04-PDF-viewer/backend/src/database/mongodb.js
+++ 04-PDF-viewer/backend/src/database/mongodb.js
@@ -4,7 +4,7 @@ import {DB_NAME} from './constant.js'
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
         console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         //console.log(connectionInstance)
         console.log("connecting",DB_NAME, process.env.MONGO_DB_URI);
